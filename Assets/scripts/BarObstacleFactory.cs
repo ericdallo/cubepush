82a19c2aca4afeb1bc9efe85071cd44b08aa6bcd
Eric Dallo <ericdallo06@hotmail.com>
2019-01-16 00:02:16 -0200

    FIx controls

---
+++ Assets/scripts/BarObstacleFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarObstacleSpawner : MonoBehaviour {
+
+    public GameObject BaseBar;
+
+    void Start() {
+        BaseBar = (GameObject) Resources.Load("prefabs/barObstacleBase", typeof(GameObject));
+    }
+
+    public GameObject Build() {
+        
+    }
+}
