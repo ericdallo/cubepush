30d8197d1f26e87d20e042004821b8c1b010bd26
Eric Dallo <ericdallo06@hotmail.com>
2019-01-21 01:06:03 -0200

    Add Stick time color change

--- Assets/Scripts/Obstacle/StickyCollider.cs
+++ Assets/Scripts/Obstacle/StickyCollider.cs
@@ -7,12 +7,32 @@ public class StickyCollider : MonoBehaviour {
     private FixedJoint joint;
     public float StickTime = 1;
     private bool canStick = true;
+    public Color StartColor;
+    public Color UnstickColor;
+    private MeshRenderer[] meshs;
+    private float lerpTime = 0;
+
+    void Start() {
+        meshs = GetComponentsInChildren<MeshRenderer>();
+    }
 
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player" && canStick) {
             joint = gameObject.AddComponent<FixedJoint>();
             joint.connectedBody = collision.rigidbody;
             Invoke("Unstick", StickTime);
+        }
+    }
+
+    void Update() {
+        if (joint != null && canStick) {
+            foreach(MeshRenderer mesh in meshs) {
+                mesh.material.color = Color.Lerp(StartColor, UnstickColor, lerpTime);
+            }
+
+            if (lerpTime < 1) {
+                lerpTime += Time.deltaTime / StickTime;
+            }
         }
     }
 
