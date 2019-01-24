8c9e8ed56e2ae50bad69686855c5bae2020fe970
Eric Dallo <ericdallo06@hotmail.com>
2019-01-23 22:35:26 -0200

    Add highScore ui

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -9,23 +9,26 @@ public class ScoreManager : MonoBehaviour {
     private int currentLevel;
     public GameObject ScoreUI;
     private TMPro.TextMeshProUGUI currentLevelText;
+    private TMPro.TextMeshProUGUI highScoreText;
 
     #region Singleton
     public static ScoreManager Instance;
     private void Awake() {
         Instance = this;
     }
+
     #endregion
 
     void Start() {
         currentLevelText = ScoreUI.transform.Find("CurrentLevel").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+        highScoreText = ScoreUI.transform.Find("HighScoreValue").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
         currentLevel = StartLevel;
     }
 
     public int NextLevel() {
         AudioManager.Instance.Play("LevelUp");
         currentLevel += 1;
-        updateUI();
+        updateCurrentLevelUI();
 
         return currentLevel;
     }
@@ -40,16 +43,29 @@ public class ScoreManager : MonoBehaviour {
 
     public void ResetLevel() {
         currentLevel = StartLevel;
-        updateUI();
+        updateCurrentLevelUI();
+        highScoreText.color = Color.white;
     }
 
     public void SaveHighScore() {
         if (PlayerPrefs.GetInt("HighScore", 0) < currentLevel) {
             PlayerPrefs.SetInt("HighScore", currentLevel);
+            highScoreText.color = Color.yellow;
         }
+
+        updateHighScoreUI();
     }
 
-    private void updateUI() {
+    public void UpdateHighScoreUI() {
+        updateHighScoreUI();
+    }
+
+    private void updateHighScoreUI() {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+    private void updateCurrentLevelUI() {
         currentLevelText.text = currentLevel.ToString();
     }
+    
 }
